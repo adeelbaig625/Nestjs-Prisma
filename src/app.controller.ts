b81f1b3f36955c86690cbc075bbf6272927be32a
adeelbaig625 <adeelbaig625@gmail.com>
2023-05-26 03:19:04 +0500

    changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello1(): string {
     return this.appService.getHello();
   }
 }

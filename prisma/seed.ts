1a230f1672cad56d6ee52e1b8380ccb75c182195
adeelbaig625 <adeelbaig625@gmail.com>
2023-05-27 13:35:32 +0500

    seeder function added

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const post1 = await prisma.article.upsert({
+    where: { title: 'Post 1' },
+    update: {},
+    create: {
+      title: 'Post 1',
+      body: 'Body 1',
+      description: 'Content 1',
+      published: true,
+    },
+  });
+  const post2 = await prisma.article.upsert({
+    where: { title: "What's new in Prisma? (Q1/22)" },
+    update: {},
+    create: {
+      title: "What's new in Prisma? (Q1/22)",
+      body: 'Our engineers have been working hard, issuing new releases with many improvements...',
+      description:
+        'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
+      published: true,
+    },
+  });
+
+  console.log({ post1, post2 });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
